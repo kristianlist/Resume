076fa3e6ffe4803e07e49eb83f7596a1d724952b
kristianlist <kl@golfbox.dk>
2024-03-22 07:18:28 +0100

    Fix for localization not beeing ready before being used

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,22 +12,22 @@
 <div
   class="min-h-screen font-sans text-secondary-800 transition-colors duration-200 ease-in-out dark:text-secondary-100"
 >
-  <!-- Cursor gradient with background color -->
-  <CursorGradient />
-  <header class="fixed left-0 right-0 top-0 flex justify-end p-4">
-    <Navbar class="z-20 w-fit" color="none">
-      <DarkMode />
-      <NavHamburger />
-      <NavUl class="md:hidden">
-        <NavLi href="#about">{$t('about')}</NavLi>
-        <NavLi href="#experience">{$t('experience')}</NavLi>
-        <NavLi href="#education">{$t('education')}</NavLi>
-        <NavLi href="#contact">{$t('contact')}</NavLi>
-      </NavUl>
-    </Navbar>
-  </header>
-
   {#if !$isLoading}
+    <!-- Cursor gradient with background color -->
+    <CursorGradient />
+    <header class="fixed left-0 right-0 top-0 flex justify-end p-4">
+      <Navbar class="z-20 w-fit" color="none">
+        <DarkMode />
+        <NavHamburger />
+        <NavUl class="md:hidden">
+          <NavLi href="#about">{$t('about')}</NavLi>
+          <NavLi href="#experience">{$t('experience')}</NavLi>
+          <NavLi href="#education">{$t('education')}</NavLi>
+          <NavLi href="#contact">{$t('contact')}</NavLi>
+        </NavUl>
+      </Navbar>
+    </header>
+
     <slot />
   {/if}
 </div>
